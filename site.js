439f2e029b2a9a16f0bfbc2db3b647703dec768a
vacunacioncovid19 <96625857+vacunacioncovid19@users.noreply.github.com>
2021-12-24 12:24:48 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,30 @@
+﻿var doc;
+doc = $(document);
+
+var Loadin = {
+
+    start: function (capa) {
+        if (capa === undefined) {
+            capa = "contenBody";
+        }
+        var ancho = $("#" + capa).width();
+        var alto = $("#" + capa).height();
+        var posicion = $("#" + capa).position();
+
+        $("#load").css({ 'top': posicion.top, 'left': posicion.left, 'height': alto, 'width': ancho });
+        $("#load").show();
+    },
+
+    stop: function () {
+        $("#load").hide();
+    }
+};
+
+
+doc.ajaxStart(function () {
+    Loadin.start();
+});
+
+doc.ajaxStop(function () {
+    Loadin.stop();
+});
